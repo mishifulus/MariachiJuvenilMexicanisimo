b2dd25e0fe4be0ff5b7ee74aabd804fe01eeed75
mishifulus <martinez.citlalli.aprogm@gmail.com>
2025-11-12 21:39:48 -0600

    Footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  facebookLink = 'https://www.facebook.com/profile.php?id=61570002355928';
+  instagramLink = 'https://www.instagram.com/juvenil_mexicanisimo';
+  tiktokLink = 'https://www.tiktok.com/@juvenil_mexicanisimo';
+  youtubeLink = 'https://www.youtube.com/@mariachijuvenilmexicanisimo';
 }
