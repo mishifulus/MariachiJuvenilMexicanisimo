aa9bc63a3e1fec9e11cab49018ee53b6c164e41d
mishifulus <martinez.citlalli.aprogm@gmail.com>
2025-11-27 21:02:28 -0600

    Finalizada

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  form = { name: '', email: '', message: '' };
+  loading = false;
+  success = false;
+  error = false;
 
+  sendEmail() {
+    this.loading = true;
+    this.success = false;
+    this.error = false;
+
+    emailjs
+      .send(
+        'service_4wa5wuj',
+        'template_v34xb0x',
+        this.form,
+        'kUQb0Wv1cojQUkW9B'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          this.success = true;
+          this.loading = false;
+          this.form = { name: '', email: '', message: '' };
+        },
+        (error) => {
+          this.error = true;
+          this.loading = false;
+        }
+      );
+  }
 }
--- src/app/features/galery/galery.component.ts
+++ src/app/features/galery/galery.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { PhotosComponent } from '../../components/photos/photos.component';
+import { VideosComponent } from "../../components/videos/videos.component";
 
 @Component({
   selector: 'app-galery',
   standalone: true,
-  imports: [],
+  imports: [PhotosComponent, VideosComponent],
   templateUrl: './galery.component.html',
   styleUrl: './galery.component.css'
 })
